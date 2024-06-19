f93bc36bcc3cac8045c702953a1fb1001f56b052
Prashant Randive <prashant.randhive05@gmail.com>
2024-06-19 19:44:30 +0530

    files Upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Weather from './components/Weather/Weather'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Weather />
+    </>
+  )
+}
+
+export default App
